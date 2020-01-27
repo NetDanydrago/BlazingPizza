6adada21b4de9f1a72589d919fce2ddff141253b
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2020-01-27 17:38:51 -0600

    Creacion de Cuadro de dialogo.

--- BlazingPizza/Server/Migrations/20200127213239_AddToppingsAndPizzas.Designer.cs
+++ BlazingPizza/Server/Migrations/20200127213239_AddToppingsAndPizzas.Designer.cs
@@ -3,14 +3,16 @@ using BlazingPizza.Server.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlazingPizza.Server.Migrations
 {
     [DbContext(typeof(PizzaStoreContext))]
-    partial class PizzaStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20200127213239_AddToppingsAndPizzas")]
+    partial class AddToppingsAndPizzas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -95,7 +97,7 @@ namespace BlazingPizza.Server.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Toppings");
+                    b.ToTable("Topping");
                 });
 
             modelBuilder.Entity("BlazingPizza.Shared.Pizza", b =>
--- BlazingPizza/Server/Models/PizzaStoreContext.cs
+++ BlazingPizza/Server/Models/PizzaStoreContext.cs
@@ -10,11 +10,26 @@ namespace BlazingPizza.Server.Models
     public class PizzaStoreContext : DbContext
     {
         public DbSet<PizzaSpecial> Specials { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=TIW1004\SQLEXPRESS;Database=PizzaStore; trusted_Connection=True;",
             providerOptions => providerOptions.CommandTimeout(300));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Definir la llave primaria de la entidad pizzaTopping
+            modelBuilder.Entity<PizzaTopping>()
+                .HasKey(pst => new { pst.PizzaId, pst.ToppingId });
+            //Una Pizza puede Tener MUCHOS Toppings.
+            modelBuilder.Entity<PizzaTopping>()
+                .HasOne<Pizza>().WithMany(ps => ps.Toppings);
+            // Un Topping Puede Estar en Muchas Pizzas.
+            modelBuilder.Entity<PizzaTopping>()
+                .HasOne(pst => pst.Topping).WithMany();
         }
     }
 }
